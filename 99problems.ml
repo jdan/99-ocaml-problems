3071cab2c83fc5d7575224df8efe4f3651756556
Jordan Scales <scalesjordan@gmail.com>
2017-07-28 16:14:32 -0400

    whoa point free

--- 99problems.ml
+++ 99problems.ml
@@ -7,7 +7,7 @@
 *)
 
 (* 1. Write a function last : 'a list -> 'a option that returns the last element of a list *)
-let rec last ls = match ls with
+let rec last = function
   | [] -> None
   | single :: [] -> Some single   (* alternatively, we can match [x] *)
   | _ :: tail -> last tail;;
@@ -17,7 +17,7 @@ assert (last [ 10 ] = Some 10);;
 assert (last [] = None);;
 
 (* 2. Find the last but one (last and penultimate) elements of a list. (easy) *)
-let rec last_two ls = match ls with
+let rec last_two = function
   | [] -> None
   | [x] -> None
   | [x ; y] -> Some (x, y)
@@ -29,22 +29,24 @@ assert (last_two [ "a" ] = None);;
 assert (last_two [] = None);;
 
 (* 3. Find the k'th element of a list. (easy) *)
-let rec at idx ls = match (ls, idx) with
-  | ([], _) -> None   (* cute that I can do this but maybe hard to read? *)
-  | (head :: _, 1) -> Some head
-  | (_ :: tail, idx) -> at (idx - 1) tail;;
+let rec at idx = function
+  | [] -> None
+  | head :: tail ->
+      if idx = 1
+      then Some head
+      else at (idx - 1) tail;;
 
 assert (at 3 [ "a" ; "b"; "c"; "d"; "e" ] = Some "c");;
 assert (at 3 [ "a" ] = None);;
 
 (* 4. Find the number of elements of a list. (easy) *)
 (* Bonus for a tail recursive solution. *)
-let rec length ls =
-  let rec inner ls acc = match ls with
+let length ls =
+  let rec inner acc = function
     | [] -> acc
-    | _ :: tail -> inner tail (acc + 1)
+    | _ :: tail -> inner (acc + 1) tail
 
-  in inner ls 0;;
+  in inner 0 ls;;
 
 assert (length [ "a" ; "b" ; "c"] = 3);;
 assert (length [] = 0);;
